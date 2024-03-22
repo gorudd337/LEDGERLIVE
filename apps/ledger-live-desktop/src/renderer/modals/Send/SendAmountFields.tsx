d6112161ccf3e478969a0226c39d9fe3b45e8104
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 18:57:55 -0500

    Initialize bugfix/LIVE-bitcoinbot

---
+++ apps/ledger-live-desktop/src/renderer/modals/Send/SendAmountFields.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Account, FeeStrategy } from "@ledgerhq/types-live";
+import { Transaction, TransactionStatus } from "@ledgerhq/live-common/generated/types";
+import { getLLDCoinFamily } from "~/renderer/families";
+
+type Props = {
+  account: Account;
+  parentAccount: Account | null | undefined;
+  transaction: Transaction;
+  status: TransactionStatus;
+  onChange: (a: Transaction) => void;
+  updateTransaction: (updater: (_: Transaction) => Transaction) => void;
+  mapStrategies?: (a: FeeStrategy) => FeeStrategy;
+  bridgePending?: boolean;
+  trackProperties?: Record<string, unknown>;
+};
+
+const AmountRelatedField = (props: Props) => {
+  const module = getLLDCoinFamily<Account, Transaction, TransactionStatus>(
+    props.account.currency.family,
+  )?.sendAmountFields;
+  if (!module) return null;
+  const Cmp = module.component;
+  return <Cmp {...props} />;
+};
+export default AmountRelatedField;
---
+++ libs/ledger-live-common/src/__tests__/mocks/account.ts
@@ -0,0 +1,42 @@
+import "../test-helpers/staticTime";
+import { genAccount } from "../../mock/account";
+import { getBalanceHistory } from "../../portfolio/v2";
+import { getEnv, setEnv } from "../../env";
+import { setSupportedCurrencies } from "../../currencies";
+setSupportedCurrencies(["ethereum", "ethereum_classic", "ripple"]);
+
+test("generate an account from seed", () => {
+  const a = genAccount("seed");
+  const b = genAccount("seed");
+  expect(a).toEqual(b);
+});
+
+test("generate an account from different seed should generate a different account", () => {
+  const a = genAccount(getEnv("MOCK"));
+  setEnv("MOCK", "çacestvraiça");
+  const b = genAccount(getEnv("MOCK"));
+  expect(a).not.toEqual(b);
+});
+
+test("dont generate negative balance", () => {
+  const a = genAccount("n"); // <= with just prando, this seed generates negative balance
+
+  expect(a.balance.isNegative()).toBe(false);
+});
+
+test("allow specifying number of operations", () => {
+  const a = genAccount("n", {
+    operationsSize: 10,
+  });
+  expect(a.operations.length).toBe(10);
+});
+
+// NB we can't guarantee that. bug in implementation because JS Numbers
+test("mock generators don't generate negative balances", () => {
+  for (let i = 0; i < 100; i++) {
+    const account = genAccount("negative?" + i);
+    const history = getBalanceHistory(account, "year", 300);
+    const invalidDataPoints = history.filter(h => h.value < 0);
+    expect(invalidDataPoints).toMatchObject([]);
+  }
+});
