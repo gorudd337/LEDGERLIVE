6ed5fd32e10afb9790e34745feab8725fc2a8111
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 18:14:49 -0500

    Initialize FEAT/live-4909-LLM-Multiselect-ANALITYCS

---
+++ apps/ledger-live-desktop/src/logger/logger-transport-main.js
@@ -0,0 +1,2 @@
+import MemoryTransport from "./logger-transport-in-memory";
+export default MemoryTransport;
---
+++ apps/ledger-live-mobile/src/screens/Manager/AppsList/AppFilter.tsx
@@ -0,0 +1,70 @@
+import React, { memo, useState, useCallback } from "react";
+import { TouchableOpacity } from "react-native";
+
+import { Icons, Box } from "@ledgerhq/native-ui";
+
+import styled from "styled-components/native";
+import { AppType, SortOptions } from "@ledgerhq/live-common/apps/filtering";
+import NotifBadge from "../NotifBadge";
+
+import FilterModalComponent from "../Modals/FilterModal";
+
+type Props = {
+  filter: string | null | undefined;
+  setFilter: (_: AppType | null | undefined) => void;
+  sort: string | null | undefined;
+  setSort: (_: SortOptions["type"] | null | undefined) => void;
+  order: string | null | undefined;
+  setOrder: (_: SortOptions["order"] | null | undefined) => void;
+  disabled?: boolean;
+};
+
+type SetStateCallback = (_: string | null | undefined) => void;
+
+const FilterButtonContainer = styled(Box).attrs({
+  width: 48,
+  height: 48,
+  borderWidth: 1,
+  borderRadius: 50,
+  alignItems: "center",
+  justifyContent: "center",
+})``;
+
+const AppFilter = ({
+  filter,
+  setFilter,
+  sort,
+  setSort,
+  order,
+  setOrder,
+  disabled,
+}: Props) => {
+  const [isOpened, setOpenModal] = useState(false);
+  const openModal = useCallback(() => setOpenModal(true), [setOpenModal]);
+  const closeModal = useCallback(() => setOpenModal(false), [setOpenModal]);
+
+  return (
+    <>
+      <TouchableOpacity disabled={disabled} onPress={openModal}>
+        <FilterButtonContainer borderColor="neutral.c40">
+          <Box>
+            <Icons.FiltersMedium size={18} color="neutral.c100" />
+            {filter !== "all" && <NotifBadge />}
+          </Box>
+        </FilterButtonContainer>
+      </TouchableOpacity>
+      <FilterModalComponent
+        isOpened={isOpened}
+        filter={filter}
+        setFilter={setFilter as SetStateCallback}
+        sort={sort}
+        setSort={setSort as SetStateCallback}
+        order={order}
+        setOrder={setOrder as SetStateCallback}
+        onClose={closeModal}
+      />
+    </>
+  );
+};
+
+export default memo(AppFilter);
