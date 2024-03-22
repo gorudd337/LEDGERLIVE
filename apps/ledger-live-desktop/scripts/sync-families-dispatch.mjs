c02896c88fc95132e8b632aa6db3806be224f5ec
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 18:47:30 -0500

    Initialize feature/PROTECT-1719-lld-sign-in-to-ledger-recover-cta-on-welcome-screen

---
+++ apps/ledger-live-desktop/scripts/sync-families-dispatch.mjs
@@ -0,0 +1,74 @@
+#!/usr/bin/env zx
+import "zx/globals";
+import rimraf from "rimraf";
+
+const basePath = path.join(__dirname, "..");
+const rendererPath = path.join(basePath, "src", "renderer");
+const generatedPath = path.join(rendererPath, "generated");
+
+await rimraf(generatedPath);
+await fs.promises.mkdir(generatedPath);
+
+const families = await fs.readdir(path.join(rendererPath, "families"));
+const targets = [
+  "operationDetails.tsx",
+  "accountActions.tsx",
+  "TransactionConfirmFields.tsx",
+  "AccountBodyHeader.ts",
+  "AccountSubHeader.tsx",
+  "SendAmountFields.tsx",
+  "SendRecipientFields.tsx",
+  "SendWarning.tsx",
+  "ReceiveWarning.tsx",
+  "AccountBalanceSummaryFooter.tsx",
+  "TokenList.tsx",
+  "AccountHeaderManageActions.ts",
+  "StepReceiveFunds.tsx",
+  "StepReceiveFundsPostAlert.tsx",
+  "NoAssociatedAccounts.tsx",
+  "live-common-setup.ts",
+  "modals.ts",
+  "StakeBanner.tsx",
+];
+
+async function genTarget(target) {
+  let imports = ``;
+  let exprts = `export default {`;
+  const outpath = path.join(generatedPath, target);
+  const [targetName, extension] = target.split(".");
+
+  for (const family of families) {
+    try {
+      if (["tsx"].includes(extension)) {
+        await Promise.any([
+          fs.promises.access(
+            path.join(rendererPath, "families", family, `${targetName}.tsx`),
+            fs.constants.R_OK,
+          ),
+        ]);
+      } else {
+        await fs.promises.access(
+          path.join(rendererPath, "families", family, target),
+          fs.constants.R_OK,
+        );
+      }
+
+      imports += `
+import ${family} from "../families/${family}/${targetName}";`;
+      exprts += `
+  ${family},`;
+    } catch (error) {}
+  }
+
+  exprts += `
+};
+`;
+
+  const str = `${imports}
+
+${exprts}`;
+
+  await fs.promises.writeFile(outpath, str, "utf8");
+}
+
+targets.map(genTarget);
---
+++ libs/ui/packages/shared/CHANGELOG.md
@@ -0,0 +1,37 @@
+# @ledgerhq/ui-shared
+
+## 0.1.10
+
+### Patch Changes
+
+- [#1141](https://github.com/LedgerHQ/ledger-live/pull/1141) [`24ea9cd15f`](https://github.com/LedgerHQ/ledger-live/commit/24ea9cd15f92d5a2c74c4b936bacb89d5d4d36fd) Thanks [@valpinkman](https://github.com/valpinkman)! - Collateral changes due to solving all LLM typescript/eslint issues/warnings.
+
+## 0.1.10-next.0
+
+### Patch Changes
+
+- [#1141](https://github.com/LedgerHQ/ledger-live/pull/1141) [`24ea9cd15f`](https://github.com/LedgerHQ/ledger-live/commit/24ea9cd15f92d5a2c74c4b936bacb89d5d4d36fd) Thanks [@valpinkman](https://github.com/valpinkman)! - Collateral changes due to solving all LLM typescript/eslint issues/warnings.
+
+## 0.1.9
+
+### Patch Changes
+
+- 0c2c6682b: ui packages - release
+
+## 0.1.9-next.0
+
+### Patch Changes
+
+- 0c2c6682b: ui packages - release
+
+## 0.1.8
+
+### Patch Changes
+
+- f686ec781: UI - shared colors - added new colors to shared themes
+
+## 0.1.8-next.0
+
+### Patch Changes
+
+- f686ec781: UI - shared colors - added new colors to shared themes
