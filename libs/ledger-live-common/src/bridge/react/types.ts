f2a0a2a1bfe5493cb8aa9fc7be34d37c631ba526
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 18:23:35 -0500

    Initialize account.selected

---
+++ libs/ledger-live-common/src/bridge/react/types.ts
@@ -0,0 +1,33 @@
+export type SyncAction =
+  | {
+      type: "BACKGROUND_TICK";
+      reason: string;
+    }
+  | {
+      type: "SET_SKIP_UNDER_PRIORITY";
+      priority: number;
+    }
+  | {
+      type: "SYNC_ONE_ACCOUNT";
+      accountId: string;
+      priority: number;
+      reason: string;
+    }
+  | {
+      type: "SYNC_SOME_ACCOUNTS";
+      accountIds: string[];
+      priority: number;
+      reason: string;
+    }
+  | {
+      type: "SYNC_ALL_ACCOUNTS";
+      priority: number;
+      reason: string;
+    };
+export type SyncState = {
+  pending: boolean;
+  error: Error | null | undefined;
+};
+export type BridgeSyncState = Record<string, SyncState>;
+// trigger an action
+export type Sync = (action: SyncAction) => void;
