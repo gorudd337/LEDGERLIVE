5dd96acff8be152f8a5271b1d445ce80b4139b23
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 18:57:54 -0500

    Initialize nft-scroll-top

---
+++ libs/ledger-live-common/src/generated/hw-getAddress.ts
@@ -0,0 +1,39 @@
+import bitcoin from "../families/bitcoin/hw-getAddress";
+import cardano from "../families/cardano/hw-getAddress";
+import celo from "../families/celo/hw-getAddress";
+import cosmos from "../families/cosmos/hw-getAddress";
+import crypto_org from "../families/crypto_org/hw-getAddress";
+import elrond from "../families/elrond/hw-getAddress";
+import ethereum from "../families/ethereum/hw-getAddress";
+import filecoin from "../families/filecoin/hw-getAddress";
+import hedera from "../families/hedera/hw-getAddress";
+import near from "../families/near/hw-getAddress";
+import ripple from "../families/ripple/hw-getAddress";
+import solana from "../families/solana/hw-getAddress";
+import stellar from "../families/stellar/hw-getAddress";
+import tezos from "../families/tezos/hw-getAddress";
+import tron from "../families/tron/hw-getAddress";
+import polkadot from "@ledgerhq/coin-polkadot/hw-getAddress";
+import algorand from "@ledgerhq/coin-algorand/hw-getAddress";
+import evm from "@ledgerhq/coin-evm/hw-getAddress";
+
+export default {
+  bitcoin,
+  cardano,
+  celo,
+  cosmos,
+  crypto_org,
+  elrond,
+  ethereum,
+  filecoin,
+  hedera,
+  near,
+  ripple,
+  solana,
+  stellar,
+  tezos,
+  tron,
+  polkadot,
+  algorand,
+  evm,
+};
