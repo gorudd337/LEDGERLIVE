d3d66dd9c511d40ee8924e3568c65df17ea53fa1
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 20:07:24 -0500

    Initialize bugfix/LIVE-6275-fix-sync-reconnection

---
+++ apps/ledger-live-mobile/src/screens/SignMessage/04-ValidationError.tsx
@@ -0,0 +1,54 @@
+import React, { useCallback, useEffect } from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { CompositeScreenProps, useTheme } from "@react-navigation/native";
+import { TrackScreen } from "../../analytics";
+import ValidateError from "../../components/ValidateError";
+
+import { ScreenName } from "../../const";
+import type { SignMessageNavigatorStackParamList } from "../../components/RootNavigator/types/SignMessageNavigator";
+import type { BaseNavigatorStackParamList } from "../../components/RootNavigator/types/BaseNavigator";
+import {
+  StackNavigatorNavigation,
+  StackNavigatorProps,
+} from "../../components/RootNavigator/types/helpers";
+
+type Navigation = CompositeScreenProps<
+  StackNavigatorProps<SignMessageNavigatorStackParamList, ScreenName.SignValidationError>,
+  StackNavigatorProps<BaseNavigatorStackParamList>
+>;
+
+export default function ValidationError({ navigation, route }: Navigation) {
+  const { colors } = useTheme();
+  const { error, onFailHandler } = route.params;
+
+  useEffect(() => {
+    if (onFailHandler && error) {
+      onFailHandler(error);
+    }
+  }, [onFailHandler, error]);
+  const onClose = useCallback(() => {
+    navigation.getParent<StackNavigatorNavigation<BaseNavigatorStackParamList>>().pop();
+  }, [navigation]);
+  const retry = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+  return (
+    <SafeAreaView
+      style={[
+        styles.root,
+        {
+          backgroundColor: colors.background,
+        },
+      ]}
+    >
+      <TrackScreen category="SignMessage" name="ValidationError" />
+      {error && <ValidateError error={error} onRetry={retry} onClose={onClose} />}
+    </SafeAreaView>
+  );
+}
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
---
+++ libs/ledger-live-common/src/hw/getOnboardingStatePolling.ts
@@ -0,0 +1,150 @@
+import { from, of, throwError, Observable, TimeoutError, timer, SchedulerLike } from "rxjs";
+import { map, catchError, first, timeout, repeat } from "rxjs/operators";
+import getVersion from "./getVersion";
+import { withDevice } from "./deviceAccess";
+import {
+  TransportStatusError,
+  DeviceOnboardingStatePollingError,
+  DeviceExtractOnboardingStateError,
+  DisconnectedDevice,
+  CantOpenDevice,
+  TransportRaceCondition,
+  LockedDeviceError,
+  UnexpectedBootloader,
+  ExchangeTimeoutError,
+} from "@ledgerhq/errors";
+import { FirmwareInfo } from "@ledgerhq/types-live";
+import { extractOnboardingState, OnboardingState } from "./extractOnboardingState";
+
+export type OnboardingStatePollingResult = {
+  onboardingState: OnboardingState | null;
+  allowedError: Error | null;
+  lockedDevice: boolean;
+};
+
+export type GetOnboardingStatePollingResult = Observable<OnboardingStatePollingResult>;
+
+export type GetOnboardingStatePollingArgs = {
+  deviceId: string;
+  pollingPeriodMs: number;
+  transportAbortTimeoutMs?: number;
+  safeGuardTimeoutMs?: number;
+  rxjsScheduler?: SchedulerLike;
+};
+
+/**
+ * Polls the device onboarding state at a given frequency
+ *
+ * @param deviceId A device id
+ * @param pollingPeriodMs The period in ms after which the device onboarding state is fetched again
+ * @param transportAbortTimeoutMs Depending on the transport implementation, an "abort timeout" will be set (and throw an error) when:
+ *  - opening a transport instance
+ *  - on called commands (where `getVersion`)
+ *  Default to (pollingPeriodMs - 100) ms
+ * @param safeGuardTimeoutMs For Transport implementations not implementing an "abort timeout", a timeout will be triggered (and throw an error) at this function call level
+ * @returns An Observable that polls the device onboarding state and pushes an object containing:
+ * - onboardingState: the device state during the onboarding
+ * - allowedError: any error that is allowed and does not stop the polling
+ * - lockedDevice: a boolean set to true if the device is currently locked, false otherwise
+ */
+export const getOnboardingStatePolling = ({
+  deviceId,
+  pollingPeriodMs,
+  transportAbortTimeoutMs = pollingPeriodMs - 100,
+  safeGuardTimeoutMs = pollingPeriodMs * 10, // Nb Empirical value
+}: GetOnboardingStatePollingArgs): GetOnboardingStatePollingResult => {
+  const getOnboardingStateOnce = (): Observable<OnboardingStatePollingResult> => {
+    return withDevice(deviceId, { openTimeoutMs: transportAbortTimeoutMs })(t =>
+      from(getVersion(t, { abortTimeoutMs: transportAbortTimeoutMs })),
+    ).pipe(
+      timeout(safeGuardTimeoutMs), // Throws a TimeoutError
+      first(),
+      catchError((error: unknown) => {
+        if (isAllowedOnboardingStatePollingError(error)) {
+          // Pushes the error to the next step to be processed (no retry from the beginning)
+          return of(error as Error);
+        }
+
+        return throwError(() => error);
+      }),
+      map((event: FirmwareInfo | Error) => {
+        if ("flags" in event) {
+          const firmwareInfo = event as FirmwareInfo;
+          let onboardingState: OnboardingState | null = null;
+
+          if (firmwareInfo.isBootloader) {
+            // Throws so it will be considered a fatal error
+            throw new UnexpectedBootloader("Device in bootloader during the polling");
+          }
+
+          try {
+            onboardingState = extractOnboardingState(firmwareInfo.flags);
+          } catch (error: unknown) {
+            if (error instanceof DeviceExtractOnboardingStateError) {
+              return {
+                onboardingState: null,
+                allowedError: error,
+                lockedDevice: false,
+              };
+            } else {
+              let errorMessage = "";
+              if (error instanceof Error) {
+                errorMessage = `${error.name}: ${error.message}`;
+              } else {
+                errorMessage = `${error}`;
+              }
+
+              return {
+                onboardingState: null,
+                allowedError: new DeviceOnboardingStatePollingError(
+                  `SyncOnboarding: Unknown error while extracting the onboarding state ${errorMessage}`,
+                ),
+                lockedDevice: false,
+              };
+            }
+          }
+          return {
+            onboardingState,
+            allowedError: null,
+            lockedDevice: false,
+          };
+        } else {
+          // If an error is caught previously, and this error is "allowed",
+          // the value from the observable is not a FirmwareInfo but an Error
+          const allowedError = event as Error;
+
+          return {
+            onboardingState: null,
+            allowedError: allowedError,
+            lockedDevice: allowedError instanceof LockedDeviceError,
+          };
+        }
+      }),
+    );
+  };
+
+  return getOnboardingStateOnce().pipe(
+    repeat({
+      delay: _count => timer(pollingPeriodMs),
+    }),
+  );
+};
+
+export const isAllowedOnboardingStatePollingError = (error: unknown): boolean => {
+  if (
+    error &&
+    // Timeout error is thrown by rxjs's timeout
+    (error instanceof TimeoutError ||
+      error instanceof ExchangeTimeoutError ||
+      error instanceof DisconnectedDevice ||
+      error instanceof CantOpenDevice ||
+      error instanceof TransportRaceCondition ||
+      error instanceof TransportStatusError ||
+      // A locked device is handled as an allowed error
+      error instanceof LockedDeviceError)
+  ) {
+    return true;
+  }
+
+  return false;
+};
