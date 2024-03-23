30d1bdf1e082d304c7a29172ca4f51fb92199a59
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 20:10:40 -0500

    Initialize support/improve-build-workflow-fields-descriptions

---
+++ apps/ledger-live-mobile/jest.config.js
@@ -0,0 +1,53 @@
+const { defaults: tsjPreset } = require("ts-jest/presets");
+
+const transformIncludePatterns = [
+  "@react-native/polyfills",
+  "(jest-)?react-native",
+  "@react-native(-community)?",
+  "rn-range-slider",
+  "react-native-reanimated",
+  "react-native-modal",
+  "react-native-animatable",
+  "@sentry/react-native",
+  "react-native-startup-time",
+  "@segment/analytics-react-native",
+  "uuid",
+];
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  ...tsjPreset,
+  verbose: true,
+  preset: "react-native",
+  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect", "./jest-setup.js"],
+  testMatch: ["**/src/**/*.test.(ts|tsx)"],
+  transform: {
+    "^.+\\.(js|jsx)?$": "babel-jest",
+    "^.+\\.(ts|tsx)?$": [
+      "ts-jest",
+      {
+        babelConfig: true,
+      },
+    ],
+  },
+  transformIgnorePatterns: [
+    `node_modules/(?!(.pnpm|${transformIncludePatterns.join("|")})/)`,
+    "\\.pnp\\.[^\\/]+$",
+  ],
+  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
+  moduleDirectories: ["node_modules"],
+  collectCoverage: false,
+  collectCoverageFrom: [
+    "src/**/*.{ts,tsx}",
+    "!src/**/*.test.{ts,tsx}",
+  ],
+  coverageReporters: ["json", "lcov", "json-summary"],
+  coverageDirectory: "<rootDir>/coverage",
+  moduleNameMapper: {
+    "^react-native/(.*)$": "<rootDir>/node_modules/react-native/$1",
+    "^react-native$": "<rootDir>/node_modules/react-native",
+    "^victory-native$": "victory",
+    "styled-components":
+      "<rootDir>/node_modules/styled-components/native/dist/styled-components.native.cjs.js",
+  },
+};
---
+++ libs/ledger-live-common/src/families/hedera/transaction.ts
@@ -0,0 +1,49 @@
+import type { Transaction, TransactionRaw } from "./types";
+import {
+  formatTransactionStatusCommon as formatTransactionStatus,
+  fromTransactionCommonRaw,
+  fromTransactionStatusRawCommon as fromTransactionStatusRaw,
+  toTransactionCommonRaw,
+  toTransactionStatusRawCommon as toTransactionStatusRaw,
+} from "@ledgerhq/coin-framework/transaction/common";
+import type { Account } from "@ledgerhq/types-live";
+import { getAccountUnit } from "../../account";
+import { formatCurrencyUnit } from "../../currencies";
+
+export function formatTransaction(transaction: Transaction, account: Account): string {
+  const amount = formatCurrencyUnit(getAccountUnit(account), transaction.amount, {
+    showCode: true,
+    disableRounding: true,
+  });
+
+  return `SEND ${amount}\nTO ${transaction.recipient}`;
+}
+
+export function fromTransactionRaw(tr: TransactionRaw): Transaction {
+  const common = fromTransactionCommonRaw(tr);
+
+  return {
+    ...common,
+    family: tr.family,
+    memo: tr.memo,
+  };
+}
+
+export function toTransactionRaw(t: Transaction): TransactionRaw {
+  const common = toTransactionCommonRaw(t);
+
+  return {
+    ...common,
+    family: t.family,
+    memo: t.memo,
+  };
+}
+
+export default {
+  formatTransaction,
+  fromTransactionRaw,
+  toTransactionRaw,
+  fromTransactionStatusRaw,
+  toTransactionStatusRaw,
+  formatTransactionStatus,
+};
