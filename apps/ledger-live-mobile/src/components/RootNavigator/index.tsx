70787a04437aa34343e94b850a8123cb219f4c6b
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 19:00:22 -0500

    Initialize bugfix/bitcoin-mooncakebot-test

---
+++ apps/ledger-live-mobile/src/components/RootNavigator/index.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import Config from "react-native-config";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigatorName } from "../../const";
+import { hasCompletedOnboardingSelector } from "../../reducers/settings";
+import BaseNavigator from "./BaseNavigator";
+import BaseOnboardingNavigator from "./BaseOnboardingNavigator";
+import { RootStackParamList } from "./types/RootNavigator";
+import { AnalyticsContext } from "../../analytics/AnalyticsContext";
+import { StartupTimeMarker } from "../../StartupTimeMarker";
+
+export default function RootNavigator() {
+  const hasCompletedOnboarding = useSelector(hasCompletedOnboardingSelector);
+  const goToOnboarding = !hasCompletedOnboarding && !Config.SKIP_ONBOARDING;
+  const [analyticsSource, setAnalyticsSource] = useState<undefined | string>(undefined);
+  const [analyticsScreen, setAnalyticsScreen] = useState<undefined | string>(undefined);
+  return (
+    <StartupTimeMarker>
+      <AnalyticsContext.Provider
+        value={{
+          source: analyticsSource,
+          screen: analyticsScreen,
+          setSource: setAnalyticsSource,
+          setScreen: setAnalyticsScreen,
+        }}
+      >
+        <Stack.Navigator
+          id={NavigatorName.RootNavigator}
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          {goToOnboarding ? (
+            <Stack.Screen name={NavigatorName.BaseOnboarding} component={BaseOnboardingNavigator} />
+          ) : null}
+          <Stack.Screen name={NavigatorName.Base} component={BaseNavigator} />
+          {hasCompletedOnboarding ? (
+            <Stack.Screen name={NavigatorName.BaseOnboarding} component={BaseOnboardingNavigator} />
+          ) : null}
+        </Stack.Navigator>
+      </AnalyticsContext.Provider>
+    </StartupTimeMarker>
+  );
+}
+const Stack = createStackNavigator<RootStackParamList>();
