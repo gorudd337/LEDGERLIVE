b0b0845af0111c8a8f7eb543976ccb7612d51e9f
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 19:19:47 -0500

    Initialize support/update-windows-swap-verify-screenshot

---
+++ libs/ledger-live-common/src/platform/providers/RemoteLiveAppProvider/api/index.ts
@@ -0,0 +1,37 @@
+import network from "@ledgerhq/live-network/network";
+import qs from "qs";
+import { getEnv } from "../../../../env";
+import { FilterParams } from "../../../filters";
+import type { LiveAppManifest } from "../../../types";
+import mockData from "./mock.json";
+
+const api = {
+  fetchLiveAppManifests: async (url: string, params?: FilterParams): Promise<LiveAppManifest[]> => {
+    if (getEnv("MOCK")) {
+      if (getEnv("MOCK_REMOTE_LIVE_MANIFEST")) {
+        return [
+          ...mockData,
+          ...JSON.parse(getEnv("MOCK_REMOTE_LIVE_MANIFEST")),
+        ] as LiveAppManifest[];
+      }
+      return mockData as LiveAppManifest[];
+    }
+    try {
+      const { data }: { data: LiveAppManifest[] } = await network({
+        method: "GET",
+        params,
+        paramsSerializer: params => {
+          return qs.stringify(params, { arrayFormat: "repeat" });
+        },
+        url,
+      });
+
+      if (!Array.isArray(data)) throw new Error("Response is not an Array");
+      return data;
+    } catch (e) {
+      console.error(e);
+      return [];
+    }
+  },
+};
+export default api;
