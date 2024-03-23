e1d3c5b12b156dbe1ad3f4adf5edcd00deeb3914
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 20:25:04 -0500

    Initialize test/moonpay-swaps

---
+++ apps/ledger-live-mobile/src/components/RootNavigator/types/SwapNavigator.ts
@@ -0,0 +1,317 @@
+import { ExchangeRate, SwapDataType } from "@ledgerhq/live-common/exchange/swap/types";
+import { CryptoCurrency, TokenCurrency } from "@ledgerhq/types-cryptoassets";
+import { Transaction } from "@ledgerhq/live-common/generated/types";
+import type { Transaction as EvmTransaction, GasOptions } from "@ledgerhq/coin-evm/types/index";
+
+import type {
+  DetailsSwapParamList,
+  DefaultAccountSwapParamList,
+  SwapSelectCurrency,
+  SwapPendingOperation,
+  SwapOperation,
+} from "../../../screens/Swap/types";
+import type {
+  CardanoAccount,
+  Transaction as CardanoTransaction,
+} from "@ledgerhq/live-common/families/cardano/types";
+import type {
+  Transaction as BitcoinTransaction,
+  TransactionStatus as BitcoinTransactionStatus,
+} from "@ledgerhq/live-common/families/bitcoin/types";
+import type {
+  AlgorandAccount,
+  AlgorandTransaction,
+  TransactionStatus as AlgorandTransactionStatus,
+} from "@ledgerhq/live-common/families/algorand/types";
+import {
+  CosmosAccount,
+  Transaction as CosmosTransaction,
+} from "@ledgerhq/live-common/families/cosmos/types";
+import {
+  CryptoOrgAccount,
+  Transaction as CryptoOrgTransaction,
+} from "@ledgerhq/live-common/families/crypto_org/types";
+import {
+  SolanaAccount,
+  Transaction as SolanaTransaction,
+} from "@ledgerhq/live-common/families/solana/types";
+import { Transaction as HederaTransaction } from "@ledgerhq/live-common/families/hedera/types";
+import type { Transaction as RippleTransaction } from "@ledgerhq/live-common/families/ripple/types";
+import type { Transaction as ICPTransaction } from "@ledgerhq/live-common/families/internet_computer/types";
+import type { Transaction as StellarTransaction } from "@ledgerhq/live-common/families/stellar/types";
+import type { Transaction as StacksTransaction } from "@ledgerhq/live-common/families/stacks/types";
+import type { Transaction as CasperTransaction } from "@ledgerhq/live-common/families/casper/types";
+import BigNumber from "bignumber.js";
+import { Account, Operation } from "@ledgerhq/types-live";
+import { ScreenName } from "../../../const";
+
+type Target = "from" | "to";
+
+export type SwapNavigatorParamList = {
+  [ScreenName.SwapTab]:
+    | DetailsSwapParamList
+    | DefaultAccountSwapParamList
+    | SwapSelectCurrency
+    | SwapPendingOperation;
+  [ScreenName.SwapSelectAccount]: {
+    target: Target;
+    provider?: string;
+    swap: SwapDataType;
+    selectableCurrencyIds: string[];
+    selectedCurrency: CryptoCurrency | TokenCurrency;
+  };
+  [ScreenName.SwapSelectCurrency]: SwapSelectCurrency;
+  [ScreenName.SwapSelectProvider]: {
+    provider?: string;
+    swap: SwapDataType;
+    selectedRate: ExchangeRate | undefined;
+  };
+  [ScreenName.SwapSelectFees]: {
+    accountId?: string;
+    parentAccountId?: string;
+    swap: SwapDataType;
+    rate?: ExchangeRate;
+    provider?: string;
+    transaction?: Transaction | null;
+    overrideAmountLabel?: string;
+    hideTotal?: boolean;
+    operation?: Operation;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.SwapPendingOperation]: SwapPendingOperation;
+  [ScreenName.SwapOperationDetails]: {
+    swapOperation: SwapOperation;
+    fromPendingOperation?: true;
+  };
+  [ScreenName.AlgorandEditMemo]: {
+    accountId?: string;
+    parentId?: string;
+    account: AlgorandAccount;
+    transaction: AlgorandTransaction;
+    status?: AlgorandTransactionStatus;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.BitcoinEditCustomFees]: {
+    accountId: string;
+    parentId?: string;
+    transaction: BitcoinTransaction;
+    status?: BitcoinTransactionStatus;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+    satPerByte?: BigNumber | null;
+    setSatPerByte?: (_: BigNumber) => void;
+  };
+  [ScreenName.CardanoEditMemo]: {
+    accountId: string;
+    parentId?: string;
+    account: CardanoAccount;
+    transaction: CardanoTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.EvmCustomFees]: {
+    accountId: string;
+    parentId?: string;
+    transaction: EvmTransaction;
+    gasOptions?: GasOptions;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.EvmEditGasLimit]: {
+    accountId: string;
+    setGasLimit: (_: BigNumber) => void;
+    gasLimit?: BigNumber | null;
+    transaction: EvmTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.RippleEditFee]: {
+    accountId: string;
+    parentId?: string;
+    transaction: RippleTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.StellarEditCustomFees]: {
+    accountId: string;
+    parentId?: string;
+    transaction: StellarTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.CosmosFamilyEditMemo]: {
+    accountId: string;
+    parentId?: string;
+    account: CosmosAccount;
+    transaction: CosmosTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.CryptoOrgEditMemo]: {
+    accountId: string;
+    parentId?: string;
+    account: CryptoOrgAccount;
+    transaction: CryptoOrgTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.HederaEditMemo]: {
+    accountId: string;
+    parentId?: string;
+    account: Account;
+    transaction: HederaTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.RippleEditTag]: {
+    accountId: string;
+    parentId?: string;
+    transaction: RippleTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.SolanaEditMemo]: {
+    accountId: string;
+    parentId?: string;
+    account: SolanaAccount;
+    transaction: SolanaTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.StellarEditMemoType]: {
+    accountId: string;
+    parentId?: string;
+    account: Account;
+    transaction: StellarTransaction;
+    memoType?: string;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.InternetComputerEditMemo]: {
+    accountId: string;
+    account: Account;
+    parentId?: string;
+    transaction: ICPTransaction;
+    currentNavigation: ScreenName.SignTransactionSummary | ScreenName.SignTransactionSummary;
+  };
+
+  [ScreenName.StacksEditMemo]: {
+    accountId: string;
+    parentId?: string;
+    account: Account;
+    transaction: StacksTransaction;
+    memoType?: string;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+  [ScreenName.CasperEditTransferId]: {
+    accountId: string;
+    account: Account;
+    parentId?: string;
+    transaction: CasperTransaction;
+    currentNavigation:
+      | ScreenName.SignTransactionSummary
+      | ScreenName.SendSummary
+      | ScreenName.SwapForm;
+    nextNavigation:
+      | ScreenName.SignTransactionSelectDevice
+      | ScreenName.SendSelectDevice
+      | ScreenName.SwapForm;
+  };
+};
---
+++ libs/ledger-live-common/src/featureFlags/index.ts
@@ -0,0 +1,10 @@
+import useFeature from "./useFeature";
+import FeatureToggle from "./FeatureToggle";
+
+export { useFeature, FeatureToggle };
+
+export * from "./defaultFeatures";
+export * from "./groupedFeatures";
+export * from "./FeatureFlagsContext";
+export * from "./useHasOverriddenFeatureFlags";
+export * from "./helper";
