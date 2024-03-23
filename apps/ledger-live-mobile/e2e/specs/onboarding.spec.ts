83c5df856f0a8c660bfccf9453b398b45131ae38
Jeremy Rudd <146176726+gorudd337@users.noreply.github.com>
2024-03-22 19:01:12 -0500

    Initialize bugfix/LIVE-7930/back-to-quote

---
+++ apps/ledger-live-mobile/e2e/specs/onboarding.spec.ts
@@ -0,0 +1,61 @@
+import { expect } from "detox";
+import { waitForElementByText } from "../helpers";
+import OnboardingSteps from "../models/onboarding/onboardingSteps";
+import PortfolioPage from "../models/wallet/portfolioPage";
+
+let onboardingSteps: OnboardingSteps;
+let portfolioPage: PortfolioPage;
+
+describe("Onboarding", () => {
+  beforeAll(() => {
+    onboardingSteps = new OnboardingSteps();
+    portfolioPage = new PortfolioPage();
+  });
+
+  it("starts Onboarding", async () => {
+    await onboardingSteps.startOnboarding();
+  });
+
+  it("selects already owned nano", async () => {
+    await onboardingSteps.DoIOwnDevice(true);
+  });
+
+  it("goes to setup my ledger nano", async () => {
+    await onboardingSteps.chooseToSetupLedger();
+    await waitForElementByText("Ledger\u00A0Nano\u00A0X");
+  });
+
+  it("chooses Nano X", async () => {
+    await onboardingSteps.selectYourDevice("Ledger\u00A0Nano\u00A0X");
+  });
+
+  it("connects to Nano X", async () => {
+    await onboardingSteps.chooseToConnectYourNano();
+    await onboardingSteps.verifyContentsOfBoxAreChecked();
+  });
+
+  it("choses to Pair Nano", async () => {
+    await onboardingSteps.chooseToPairMyNano();
+  });
+
+  it("selects Pair with Bluetooth", async () => {
+    await onboardingSteps.selectPairWithBluetooth();
+  });
+
+  //FIXME Fix device action mocks B2CQA-1834
+  it.skip("adds device via Bluetooth", async () => {
+    await onboardingSteps.addDeviceViaBluetooth("David");
+  });
+
+  //FIXME Fix device action mocks B2CQA-1834
+  it.skip("opens Ledger Live", async () => {
+    await onboardingSteps.openLedgerLive();
+    await portfolioPage.waitForPortfolioPageToLoad();
+    await expect(portfolioPage.portfolioSettingsButton()).toBeVisible();
+  });
+
+  //FIXME Fix device action mocks B2CQA-1834
+  it.skip("should see an empty portfolio page", async () => {
+    await expect(portfolioPage.emptyPortfolioComponent()).toBeVisible();
+  });
+});
---
+++ libs/ledger-live-common/src/notifications/ServiceStatusProvider/api/api.ts
@@ -0,0 +1,21 @@
+import network from "@ledgerhq/live-network/network";
+import { getEnv } from "../../../env";
+import type { ServiceStatusApi, ServiceStatusSummary } from "../types";
+
+const baseStatusUrl = () => getEnv("STATUS_API_URL");
+
+const statusVersion = () => getEnv("STATUS_API_VERSION");
+
+async function fetchStatusSummary(): Promise<ServiceStatusSummary> {
+  const url = `${baseStatusUrl()}/v${statusVersion()}/summary.json`;
+  const { data } = await network({
+    method: "GET",
+    url,
+  });
+  return data;
+}
+
+const api: ServiceStatusApi = {
+  fetchStatusSummary,
+};
+export default api;
